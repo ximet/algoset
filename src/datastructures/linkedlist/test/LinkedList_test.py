24b50fa19179f2635786e13b15a4d35f32dbef4f
ximet <dq.ximet@gmail.com>
2020-07-11 11:15:21 +0300

    test for iteration

--- src/datastructures/linkedlist/test/LinkedList_test.py
+++ src/datastructures/linkedlist/test/LinkedList_test.py
@@ -1,5 +1,7 @@
 from src.datastructures.linkedlist.LinkedList import LinkedList
 
+# append tests
+
 def test_linkedListWithOneNode():
     linkedList = LinkedList()
     linkedList.append(1)
@@ -13,6 +15,8 @@ def test_LinkedListWithTwoNode():
 
     assert linkedList.head.value == 1
     assert linkedList.tail.value == 2
+
+# remove tests
 
 def test_LinkedListRemoveFirstNode():
     linkedList = LinkedList()
@@ -28,3 +32,15 @@ def test_LinkedListRemoveSecondValue():
     linkedList.remove(2)
     assert linkedList.head.value == 1
     assert linkedList.head.value == linkedList.tail.value
+
+# iteration test
+
+def test_iteration():
+    linkedList = LinkedList()
+    linkedList.append(1)
+    linkedList.append(2)
+    linkedList.append(3)
+    counter = 1
+    for node in linkedList:
+        assert node.value == counter
+        counter += 1
